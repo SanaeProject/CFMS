f6e5bd27cd2640a8ab5f0305d4e5027097c00393
OtonariShunji <shunji@otonari.dev>
2026-07-30 17:08:00 +0900

    Phinx,ormの下準備を行います。

---
+++ backend/phinx.php
@@ -0,0 +1,42 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'production' => [
+            'adapter' => $_ENV["DB_CONNECTION"],
+            'host' => $_ENV["DB_HOST"],
+            'name' => $_ENV["DB_NAME"],
+            'user' => $_ENV["DB_USER"],
+            'pass' => $_ENV["DB_PASSWORD"],
+            'port' => $_ENV["DB_PORT"],
+            'charset' => $_ENV["DB_CHARSET"],
+        ],
+        'development' => [
+            'adapter' => $_ENV["DB_CONNECTION"],
+            'host' => $_ENV["DB_HOST"],
+            'name' => $_ENV["DB_NAME"],
+            'user' => $_ENV["DB_USER"],
+            'pass' => $_ENV["DB_PASSWORD"],
+            'port' => $_ENV["DB_PORT"],
+            'charset' => $_ENV["DB_CHARSET"],
+        ],
+        'testing' => [
+            'adapter' => $_ENV["DB_CONNECTION"],
+            'host' => $_ENV["DB_HOST"],
+            'name' => $_ENV["DB_NAME"],
+            'user' => $_ENV["DB_USER"],
+            'pass' => $_ENV["DB_PASSWORD"],
+            'port' => $_ENV["DB_PORT"],
+            'charset' => $_ENV["DB_CHARSET"],
+        ]
+    ],
+    'version_order' => 'creation'
+];
