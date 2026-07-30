40420e9a07a66180b96bc854d8de04f3fe692c45
OtonariShunji <shunji@otonari.dev>
2026-07-30 17:21:02 +0900

    修正します。

--- backend/db/bootstrap.php
+++ backend/db/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../../");
 $dotenv->load();
 
 $db = new Illuminate\Database\Capsule\Manager();
 $db->addConnection([
-    'driver' => 'mariadb',
+    'driver' => $_ENV['DB_CONNECTION'],
     'host' => $_ENV['DB_HOST'],
     'port' => $_ENV['DB_PORT'],
     'database' => $_ENV['DB_NAME'],
